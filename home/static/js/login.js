80800efcb4f1a31067b6f66fbfd589074f404009
Anupam Anand <thewarlord858@gmail.com>
2019-03-30 15:44:37 +0530

    fully functional home design

---
+++ home/static/js/login.js
@@ -0,0 +1,14 @@
+<script>
+
+var modal = document.getElementById('id01');
+
+
+ window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+  function borderColor(elmt,color){
+	elmt.style.borderColor = color;
+	}
+</script>
